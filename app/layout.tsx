c673b03998615848ec9fa650eb9b3748d0716384
Jonatan Hartmann <jonatanhartamann@gmail.com>
2024-01-16 19:49:07 -0300

    feat(layout): adds navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import '@/app/ui/globals.css'
+import Navbar from './ui/navbar'
 
 export const metadata: Metadata = {
   title: 'JOJOS',
@@ -13,7 +14,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <div className="container md:px-20 px-10 md:py-10 py-5">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
---
+++ app/ui/navbar.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { montserrat, press } from '@/app/ui/fonts';
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from '@/app/ui/button';
+import styles from '@/app/ui/button.module.css'
+import { redirect } from 'next/navigation';
+
+export default function Navbar() {
+  return (
+    <nav className='md:flex px-20 py-5 md:justify-between w-full md:content-center'>
+        <div id='logo' className='flex content-center md:justify-around flex-wrap-reverse justify-center'>
+          <Image
+              src="/Icon.png"
+              width={49}
+              height={40}
+              alt="Logotipo"
+              className='block mr-2'
+          />
+          <div className={`${press.className} content-center flex h-min flex-wrap-reverse`}>
+              <span className='font-white text-2xl'>J</span>
+              <span className='font-sunrise text-2xl'>O</span>
+              <span className='font-white text-2xl'>J</span>
+              <span className='font-sunrise text-2xl'>O</span>
+              <span className='font-white text-2xl'>S</span>
+          </div>
+        </div>
+        <div id='menu' className={`${montserrat.className} flex my-3 md:my-auto justify-between md:w-2/6 w-full content-center items-center`}>
+          <Link href={'/#jogos'} className='font-white h-min'>Jogos</Link>
+          <Link href={'/#sobre'} className='font-white h-min'>Sobre</Link>
+          <Link href={'/blog'} className='font-white h-min'>Blog</Link>
+          <Link href={'/#carreira'} className={`${styles.btn} p-2 w-50`}>Carreira</Link>
+        </div>
+    </nav>
+  )
+}
