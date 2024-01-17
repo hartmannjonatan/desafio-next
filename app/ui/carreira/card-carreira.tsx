bbaedf410ecdfc979da413a15fb6bb7464de345b
Jonatan Hartmann <jonatanhartamann@gmail.com>
2024-01-17 13:52:52 -0300

    refactor(card-carreira): adds key prop in card categories

--- app/ui/carreira/card-carreira.tsx
+++ app/ui/carreira/card-carreira.tsx
@@ -15,7 +15,7 @@ export default function CardCarreira({
             <h2 className={`${montserrat.className} text-start m-2 font-bold font-gray mb-2`}>{title}</h2>
             <div className={`${styles.categories} flex justify-start content-center items-center`}>
                 {categories.map((category, i) => {
-                    return <div className={`${montserrat.className} ${styles.category} p-1 m-2 font-gray`}>{category}</div>
+                    return <div key={i} className={`${montserrat.className} ${styles.category} p-1 m-2 font-gray`}>{category}</div>
                 })}
             </div>
         </div>
