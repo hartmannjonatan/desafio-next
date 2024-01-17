9d337df19503f508817a6fa4ab9a8c6f4dde74c5
Jonatan Hartmann <jonatanhartamann@gmail.com>
2024-01-17 13:06:28 -0300

    feat(home): adds Sobre Nós section

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,25 @@ export default function Home() {
             </div>
         </div>
       </div>
-      <div className="h-screen" id='sobre'></div>
+      <div className="w-full mt-14" id='sobre'>
+        <h1 className={`${press.className} font-sunrise text-center w-full text-2xl`}>Sobre Nós</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20 mt-3">
+          <div className={`${montserrat.className} text-center font-white`}>
+            <p className='py-5'>
+              Bem-vindo à Jojos, uma empresa de jogos retrô brasileira! Somos uma equipe apaixonada por games clássicos e estamos comprometidos em trazer de volta a magia desses títulos que marcaram época. Com gráficos pixelizados, trilhas sonoras envolventes e mecânicas desafiadoras, nossos jogos são verdadeiras homenagens aos consoles e computadores que encantaram o passado. Junte-se a nós e embarque em uma viagem nostálgica repleta de aventuras.
+            </p>
+            <p className='py-5'>
+              Acreditamos que os jogos retrô têm o poder de unir gerações, despertar memórias afetivas e proporcionar momentos de pura diversão. Nossa missão é manter viva a essência dos jogos clássicos, levando você a uma jornada inesquecível pelos mundos pixelizados cheios de magia. Faça parte dessa nova era retrô e mergulhe em experiências que continuam a encantar corações até hoje.
+            </p>
+            <p className='py-5'>
+              Jojos, onde a nostalgia encontra a diversão! Com uma equipe apaixonada por games, estamos comprometidos em trazer de volta a magia dos jogos clássicos que marcaram gerações inteiras. Explore nossos jogos e embarque em uma viagem nostálgica repleta de aventuras e desafios. Junte-se a nós e compartilhe da nossa paixão pelos jogos retrô!
+            </p>
+          </div>
+          <div>
+            <Image alt='Imagem sobre nós' src={"/image-about_us.png"} width={560} height={689} />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
