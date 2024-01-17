097b853245a4a3f73d5fcd8f0551d9c8a9005f06
Jonatan Hartmann <jonatanhartamann@gmail.com>
2024-01-17 13:48:42 -0300

    fix(navbar): fix logo button link

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -6,22 +6,24 @@ import styles from '@/app/ui/button.module.css'
 export default function Navbar() {
   return (
     <nav className='md:flex md:px-20 px-5 py-5 md:justify-between w-full md:content-center'>
-        <div id='logo' className='flex content-center md:justify-around flex-wrap-reverse justify-center'>
-          <Image
-              src="/Icon.png"
-              width={49}
-              height={40}
-              alt="Logotipo"
-              className='block mr-2'
-          />
-          <div className={`${press.className} content-center flex h-min flex-wrap-reverse`}>
-              <span className='font-white text-2xl'>J</span>
-              <span className='font-sunrise text-2xl'>O</span>
-              <span className='font-white text-2xl'>J</span>
-              <span className='font-sunrise text-2xl'>O</span>
-              <span className='font-white text-2xl'>S</span>
+        <Link href={"/"}>
+          <div id='logo' className='flex content-center md:justify-around flex-wrap-reverse justify-center'>
+            <Image
+                src="/Icon.png"
+                width={49}
+                height={40}
+                alt="Logotipo"
+                className='block mr-2'
+            />
+            <div className={`${press.className} content-center flex h-min flex-wrap-reverse`}>
+                <span className='font-white text-2xl'>J</span>
+                <span className='font-sunrise text-2xl'>O</span>
+                <span className='font-white text-2xl'>J</span>
+                <span className='font-sunrise text-2xl'>O</span>
+                <span className='font-white text-2xl'>S</span>
+            </div>
           </div>
-        </div>
+        </Link>
         <div id='menu' className={`${montserrat.className} flex my-3 md:my-auto justify-between md:w-2/6 w-full content-center items-center`}>
           <Link href={'/#jogos'} className='font-white h-min'>Jogos</Link>
           <Link href={'/#sobre'} className='font-white h-min'>Sobre</Link>
