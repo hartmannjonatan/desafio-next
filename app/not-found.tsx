de16bb03fe355deb3b90d977919cf0f643eb32e6
Jonatan Hartmann <jonatanhartamann@gmail.com>
2024-01-18 16:50:53 -0300

    feat(notfound): adds not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import { montserrat, press } from '@/app/ui/fonts';
+
+
+export default async function NotFound() {
+  return (
+    <>
+      <h2 className={`${montserrat.className} font-white font-bold text-xl`}>Página não encontrada.</h2>
+    </>
+  )
+}
